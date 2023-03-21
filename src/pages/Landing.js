278d208aadb2edb2886e27d14d45a78615134f4d
ajitjadhav701 <ajitjadhav701@gmail.com>
2023-03-21 22:29:31 +0530

    add react routing and done with Error page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,12 +1,12 @@
-
-import logo from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
-import styled from 'styled-components';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/LandingPage';
 const Landing = () => {
     return (
         <Wrapper>
             <nav>
-                <img src={logo} alt="jobster logo" className="logo" />
+                <Logo />
             </nav>
             <div className="container page">
                 {/* info */}
@@ -15,7 +15,7 @@ const Landing = () => {
                         job <span>tracking</span> app
                     </h1>
                     <p>Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up fixie raclette taxidermy craft beer. Brunch bitters synth, VHS crucifix heirloom meggings bicycle rights.</p>
-                    <button className="btn btn-hero">Login/Register</button>
+                    <Link to="/register" className="btn btn-hero">Login/Register</Link>
                 </div>
                 <img src={main} alt="job hunt" className="img main-img" />
             </div>
@@ -23,43 +23,5 @@ const Landing = () => {
     );
 };
 
-const Wrapper = styled.main`
-nav{
-    width:var(--fluid-width);
-    max-width:var(--max-width);
-    margin:0 auto;
-    height:var(--nav-height);
-    display:flex;
-    align-items:center;
-}
-.page{
-    min-height:calc(100vh-var(--nav-height));
-    display:grid;
-    align-items:center;
-    margin-top:-3 rem;
-}
-h1{
-    font-weight:700;
-    span{
-        color:var(--primary-500);
-}
-}
-p{
-    color:var(--gray-600);
-}
-.main-img{
-    display:none;
-}
-@media (min-width:992px){
-    .page{
-        grid-template-columns:1fr 1fr;
-        column-gap:3 rem;
-    }
-    .main-img{
-        display:block;
-    }
-}
-
-`;
 
 export default Landing;
